efd3f7ecc1e4b836f9739524f521c22529df07e8
Sean Perez <sean.perez29@gmail.com>
2017-02-15 21:23:00 -0700

    Add UI layouts

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Sean Perez on 2/15/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
